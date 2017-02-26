e7d40632c6431a914a78b2b15a6a2a01143132e2
zhangzhaoyu <zhangzhaoyu0524@163.com>
2017-02-26 22:18:28 +0800

    init account project

---
+++ account-web/src/main/java/cn/cincout/cavia/cloud/accout/AccountWebApplication.java
@@ -0,0 +1,12 @@
+package cn.cincout.cavia.cloud.accout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountWebApplication.class, args);
+	}
+}
